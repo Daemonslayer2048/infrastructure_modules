451e255e614aad37cae981114844b91f25b93f30
Jacob Hanafin <jacob@hanafin.pro>
2021-10-06 22:22:22 -0600

    Minimum working example of proxied-vm

---
+++ proxied-vm/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    // For notes on how to install this provider: https://github.com/Telmate/terraform-provider-proxmox/blob/master/docs/guides/installation.md
+    proxmox = {
+      source = "telmate/proxmox"
+      version = ">=2.7.4"
+    }
+    unifi = {
+      source = "paultyng/unifi"
+      version = ">=0.30.1"
+    }
+    namecheap = {
+      source = "namecheap/namecheap"
+      version = ">= 2.0.0"
+    }
+  }
+}
